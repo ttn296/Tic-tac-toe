a0271c630d093737fc01c813aa13a59fc7bc3094
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-03-30 14:25:00 -0400

    make X and O on the board

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-const gameboard = document.querySelector('#gameboard');
+const gameBoard = document.querySelector('#gameBoard');
 const infoDisplay = document.querySelector('#info');
 const startCells = [
     "", "", "", "", "", "", "", "", ""
 ]
 
+let current_player = "circle"
+infoDisplay.textContent = "Circle goes first"
+
 // create the board
 function createBoard(){
-startCells.forEach((cell, index) =>{
+startCells.forEach((_cell, index) =>{
     const cellElement = document.createElement('div')
     cellElement.classList.add('square')
-    gameboard.append(cellElement);
+    cellElement.id = index
+    cellElement.addEventListener('click', addGo)
+    gameBoard.append(cellElement);
 })
 }
 createBoard();
+
+//Add X or O to the empty board
+function addGo(ev) {
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add('cross')
+    ev.target.append(goDisplay)
+
+}
